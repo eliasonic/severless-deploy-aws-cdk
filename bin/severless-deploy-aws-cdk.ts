fc57de3a0431eaa4e23eacd53e0c4b7dc54f767b
Elias Adjetey <eliasonics@gmail.com>
2024-06-20 01:23:22 +0000

    deploy severless application to aws with cdk

--- bin/severless-deploy-aws-cdk.ts
+++ bin/severless-deploy-aws-cdk.ts
@@ -2,6 +2,8 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { SeverlessDeployAwsCdkStack } from '../lib/severless-deploy-aws-cdk-stack';
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const app = new cdk.App();
 new SeverlessDeployAwsCdkStack(app, 'SeverlessDeployAwsCdkStack', {
@@ -11,7 +13,8 @@ new SeverlessDeployAwsCdkStack(app, 'SeverlessDeployAwsCdkStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  stage: process.env.STAGE!,
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
--- lib/severless-deploy-aws-cdk-stack.ts
+++ lib/severless-deploy-aws-cdk-stack.ts
@@ -1,16 +1,90 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path'
+
+export interface LambdaProps extends cdk.StackProps {
+  stage: string
+}
 
 export class SeverlessDeployAwsCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: LambdaProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { stage } = props
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SeverlessDeployAwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // The code that defines your stack goes here
+    const pomDynamodbTable = new cdk.aws_dynamodb.Table(this, `pom-dynamodb-${stage}`, {
+      tableName: 'pom-dynamodb',
+      partitionKey: { name: 'ID', type: cdk.aws_dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
+
+    const pomLambdaFunc = new cdk.aws_lambda.Function(this, `pom-lambda-${stage}`, {
+      functionName: 'pom-lambda',
+      runtime: cdk.aws_lambda.Runtime.NODEJS_16_X,
+      code: cdk.aws_lambda.Code.fromAsset(path.join(__dirname, '..', 'src')),
+      handler: 'index.handler',
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(5),
+      logRetention: cdk.aws_logs.RetentionDays.FIVE_DAYS,
+      environment: {
+        STAGE: stage
+      },
+      initialPolicy: [
+        new cdk.aws_iam.PolicyStatement({
+          effect: cdk.aws_iam.Effect.ALLOW,
+          actions: ['dynamodb:*'],
+          resources: [pomDynamodbTable.tableArn]
+        })
+      ]
+    })
+
+    // dynamodbTable.grantReadWriteData(lambdaFunc)
+    
+    // const pomApiGatewayProxy = new cdk.aws_apigateway.LambdaRestApi(this, `pom-apigateway-proxy-${stage}`, {
+    //   handler: pomLambdaFunc,
+    //   proxy: true,
+    //   binaryMediaTypes: ['*/*'],
+    //   deploy: true,
+    //   deployOptions: {
+    //     stageName: stage
+    //   }
+    // })
+
+    // Api Gateway with Explicit Resource and Method
+    const pomApiGateway = new cdk.aws_apigateway.RestApi(this, `pom-apigateway-${stage}`, {
+      restApiName: 'pom-apigateway',
+      defaultCorsPreflightOptions: {
+        allowOrigins: cdk.aws_apigateway.Cors.ALL_ORIGINS
+      }
+    })
+
+    const postLambdaIntegration = new cdk.aws_apigateway.LambdaIntegration(pomLambdaFunc)
+
+    const calc = pomApiGateway.root.addResource('calc')
+    calc.addMethod('POST', postLambdaIntegration)
+    
+
+    // CloudFormation Outputs
+    new cdk.CfnOutput(this, 'pomDynamodbTable', {
+      exportName: 'pomDynamodbTable-arn',
+      value: pomDynamodbTable.tableArn
+    })
+
+    new cdk.CfnOutput(this, 'pomLambdaFunc', {
+      exportName: 'pomLambdaFunc-arn',
+      value: pomLambdaFunc.functionArn
+    })
+
+    // new cdk.CfnOutput(this, 'pomApiGatewayProxy', {
+    //   exportName: 'pomApiGatewayProxy-arn',
+    //   value: pomApiGatewayProxy.restApiName
+    // })
+
+    new cdk.CfnOutput(this, 'pomApiGateway', {
+      exportName: 'pomApiGateway-arn',
+      value: pomApiGateway.restApiName
+    })
   }
 }
